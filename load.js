4d6a1e782c33ec3582f74d266c4182acd0f86107
ngewekadal <39780075+ngewekadal@users.noreply.github.com>
2018-09-19 14:10:08 +0700

    Create load.js

---
+++ load.js
@@ -0,0 +1,21 @@
+var secs = 20; // Süre
+var wait = secs * 1000;
+document.countdownhc.submitbtn.disabled=true;
+for(i=1;i<=secs;i++) {
+window.setTimeout("update(" + i + ")", i * 1000);
+}
+window.setTimeout("timer()", wait);
+function update(num) {
+
+if(num == (wait/1000)) {
+document.countdownhc.submitbtn.value = "Download";
+}
+
+else {
+printnr = (wait/1000)-num;
+document.countdownhc.submitbtn.value = "Wait " + printnr + " seconds";
+}}
+
+function timer() {
+document.countdownhc.submitbtn.disabled=false;
+}
